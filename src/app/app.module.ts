3b12369fc5ea51a3ab81f3ebcf19937f4c6ae646
Andrey Koval <akoval54@gmail.com>
2018-09-17 13:17:48 +0700

    auto-generate SearchBoxComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBoxComponent
   ],
   imports: [
     BrowserModule
