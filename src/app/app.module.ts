0c34f9bd0af4ba51349391ff7a4b3a5585d6413b
Andrey Koval <akoval54@gmail.com>
2018-09-18 15:48:02 +0700

    add domains component skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SearchBoxComponent } from './search-box/search-box.component';
+import { DomainsComponent } from './domains/domains.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    DomainsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/domains/domains.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-domains',
+  templateUrl: './domains.component.html',
+  styleUrls: ['./domains.component.scss']
+})
+export class DomainsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
