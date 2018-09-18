6f4a5529d372ddddf458b7aca4cab87d3f124851
Andrey Koval <akoval54@gmail.com>
2018-09-18 12:08:16 +0700

    add min/max tld length calculation

--- download-tld-list.sh
+++ download-tld-list.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
 curl https://tld-list.com/df/tld-list-details.json > src/assets/tld-list.json
+
+cat >src/app/tldlength.ts <<EOL
+export const TLD_MIN_LENGTH = 2;
+export const TLD_MAX_LENGTH = $(curl -s https://tld-list.com/df/tld-list-basic.txt | wc -L);
+EOL
---
+++ src/app/tldlength.ts
@@ -0,0 +1,2 @@
+export const TLD_MIN_LENGTH = 2;
+export const TLD_MAX_LENGTH = 18;
