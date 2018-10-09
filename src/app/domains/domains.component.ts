66f22fc46b877f1eb4e91bae129f971d89f6012b
Andrey Koval <akoval54@gmail.com>
2018-10-09 20:12:21 +0700

    hide TLD manager column on mobile devices
    responsive ui improvements

--- src/app/domains/domains.component.ts
+++ src/app/domains/domains.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 import {DomainService} from 'src/app/domain.service';
 import DomainHack from 'src/app/domain-hack';
@@ -10,12 +11,20 @@ import DomainHack from 'src/app/domain-hack';
 })
 export class DomainsComponent implements OnInit {
   domainHacks: DomainHack[];
+  columns: string[];
 
-  constructor(private domainService: DomainService) {
+  constructor(private domainService: DomainService, private breakpointObserver: BreakpointObserver) {
     domainService.domainHacks$.subscribe(
       domainHacks => {
         this.domainHacks = domainHacks.toJS();
       });
+
+    // Hide TLD manager column on mobile devices (width: 0-599px).
+    breakpointObserver.observe([Breakpoints.XSmall]).subscribe(result => {
+      this.columns = result.matches
+        ? ['domain', 'country']
+        : ['domain', 'manager', 'country'];
+    });
   }
 
   ngOnInit() {
