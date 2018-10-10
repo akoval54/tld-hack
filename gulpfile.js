9acdd5d601a66d3be7d10f82f36764ae2da8f42d
Andrey Koval <akoval54@gmail.com>
2018-10-10 16:16:12 +0700

    add zip dist logic

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var gzip = require('gulp-gzip');
+var zip = require('gulp-zip');
 
-gulp.task('compress', () => {
+gulp.task('compress-files', () => {
   gulp.src(['./dist/**/*.*'])
     .pipe(gzip())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('compress-dist', () => {
+  gulp.src(['./dist/domains/**'])
+    .pipe(zip('domains.zip'))
     .pipe(gulp.dest('./dist'));
 });
