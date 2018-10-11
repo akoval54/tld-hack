261f07adba4a3f04b1207b407d0c010ff07fbfb0
Andrey Koval <akoval54@gmail.com>
2018-10-11 18:01:25 +0700

    fix auto-generated e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display slogan', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to domains!');
+    expect(page.getSloganText()).toEqual('Doma.in Ha.ck Sear.ch');
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,11 @@
-import { browser, by, element } from 'protractor';
+import {browser, by, element} from 'protractor';
 
 export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
+  getSloganText = () => element.all(by.css('.b-header-slogan__word'))
+    .reduce((acc, elem) => elem.getText().then(text => `${acc}${text} `), '')
+    .then(slogan => slogan.trim())
 }
